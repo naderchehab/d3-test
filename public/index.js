c0849359fc8329a36c91022b878ef7d49441df50
Nader Chehab <naderchehab@gmail.com>
2016-06-13 22:43:38 -0700

    Added ability to update chart

--- public/index.js
+++ public/index.js
@@ -1,16 +1,37 @@
 var data = [4, 8, 100, 16, 23, 42];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 1000]);
+draw();
+bindEvents();
 
-d3.select(".chart")
-    .selectAll("div")
-    .data(data)
-    .enter().append("div")
-    .style("width", function(d) {
-        return x(d) + "px";
-    })
-    .text(function(d) {
-        return d;
+function draw() {
+    var x = d3.scale.linear()
+        .domain([0, 100])
+        .range([0, 1000]);
+
+    var chart = d3.select('.chart')
+        .selectAll('div')
+        .data(data);
+
+    chart.enter()
+        .append('div');
+
+    chart.style('width', function(d) {
+            return x(d) + 'px';
+        })
+        .text(function(d) {
+            return d;
+        });
+}
+
+function bindEvents() {
+    var button = document.querySelector('button');
+    button.addEventListener('click', buttonClick);
+}
+
+function buttonClick() {
+    data = data.map(function() {
+        var val = Math.floor(Math.random() * 100);
+        return val;
     });
+    draw();
+}
